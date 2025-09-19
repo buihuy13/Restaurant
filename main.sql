487b3fed4fbb05c6b05777e655029edddc3d6eb3
buihuy13 <buiquochuytd@gmail.com>
2025-09-19 22:46:39 +0700

    init restaurant-service

--- main.sql
+++ main.sql
@@ -3,17 +3,84 @@ create database if not exists `user-service`;
 use `user-service`;
 
 create table users (
-id varchar(255) primary key,
-username varchar(255) not null,
-email varchar(255) not null unique,
-`password` varchar(255) not null,
-`role` varchar(10) not null,
-`enabled` boolean not null,
-verificationcode varchar(255) not null 
-phone varchar(15) not null);
+    id varchar(255) primary key,
+    username varchar(255) not null,
+    email varchar(255) not null unique,
+    `password` varchar(255) not null,
+    `role` varchar(10) not null,
+    `enabled` boolean not null,
+    verificationcode varchar(255) not null,
+    phone varchar(15) not null
+);
 
 create table address (
-id varchar(255) primary key,
-location varchar(255) not null,
-user_id varchar(255) not null references users(id)
+    id varchar(255) primary key,
+    location varchar(255) not null,
+    user_id varchar(255) not null references users(id)
+);
+
+drop database if exists `restaurant-service`;
+create database if not exists `restaurant-service`;
+use `restaurant-service`;
+
+create table categories (
+    id varchar(10) primary key,
+    name varchar(100) not null unique
+);
+
+create table restaurants (
+    id varchar(255) primary key,
+    `name` varchar(255) not null,
+    `address` varchar(255) not null,
+    rating float not null,
+    openingtime time not null,
+    closingtime time not null,
+    `imagename` varchar(255) not null,
+    `imagetype` varchar(50) not null,
+    `imagedata` longblob not null,
+    merchant_id varchar(255) not null 
+);
+
+create table restaurant_categories (
+    id varchar(255) primary key,
+    restaurant_id varchar(255) not null references restaurants(id),
+    category_id varchar(10) not null references categories(id),
+    UNIQUE KEY unique_restaurant_category (restaurant_id, category_id)
+);
+
+create table size (
+    id varchar(10) primary key,
+    name varchar(50) not null unique
+);
+
+create table products (
+    id varchar(255) primary key,
+    name varchar(255) not null,
+    `description` text not null,
+    price decimal not null,
+    restaurant_id varchar(255) not null references restaurants(id),
+    imagename varchar(255) not null,
+    imagetype varchar(50) not null,
+    imagedata longblob not null,
+    category_id varchar(10) not null references categories(id),
+    volume int not null,
+    available boolean not null
+);
+
+create table product_sizes (
+    id varchar(255) primary key,
+    product_id varchar(255) not null references products(id),
+    size_id varchar(10) not null references size(id),
+    UNIQUE KEY unique_product_size (product_id, size_id)
+);
+
+create table reviews (
+    id varchar(200) primary key,
+    title varchar(255) not null,
+    content text not null,
+    rating float not null,
+    review_id varchar(255) not null,
+    review_type varchar(20) not null,
+    created_at timestamp default current_timestamp,
+    user_id varchar(255) not null
 );
---
+++ restaurant-service/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+	id("org.springframework.boot")
+}
+
+extra["springCloudVersion"] = "2025.0.0"
+
+dependencies {
+	implementation(project(":Common"))
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    implementation("io.github.cdimascio:dotenv-java:3.2.0")
+    //implementation("org.springframework.boot:spring-boot-starter-actuator")
+    //implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    //implementation("io.zipkin.reporter2:zipkin-reporter-brave")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
