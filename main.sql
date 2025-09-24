601f3de1228ee941cda76514cfe2df28fa846c02
buihuy13 <buiquochuytd@gmail.com>
2025-09-24 20:46:16 +0700

    add data to mysql

--- main.sql
+++ main.sql
@@ -13,6 +13,10 @@ create table users (
     phone varchar(15) not null
 );
 
+insert into users(id, `password`, username, email, phone, `role`, `enabled`, `verificationcode`) values 
+("testadminid", "$2a$12$xv4.GmxuJeUUs54wJNwPdODdcvnHs7ikvpCuLeVVMy4tki5hZLq/m", "testadmin", "testadmin@gmail.com", "0762612698", "ADMIN", true, "abcxyz123"),
+("testuserid", "$2a$12$CydeMvJj1Hvu/824Lh2NuOEIrZnlhRMIUM736cYXa7bSD3LUmGW7K", "testuser", "testuser@gmail.com", "0762612699", "USER", true, "abcxyz456");
+
 create table address (
     id varchar(255) primary key,
     `location` varchar(255) not null,
