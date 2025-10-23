7bb1b55156631eee2a18dd4e6856add7f8fbda13
caotrinhthinh <caotrinhthinh@gmail.com>
2025-10-23 23:59:02 +0700

    Add routes

--- order-service/src/routes/orderRoutes.js
+++ order-service/src/routes/orderRoutes.js
@@ -5,6 +5,44 @@ import { authenticate, authorize } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // Create order
-router.post("/", authenticate, orderController.createOrder);
+router.post("/", authenticate, authorize("USER"), orderController.createOrder);
+
+// Get order by Id
+router.get("/:orderId", authenticate, orderController.getOrderById);
+
+// Get user orders
+router.get(
+  "/user/:userId",
+  authenticate,
+  authorize("USER", "ADMIN"),
+  orderController.getUserOrders
+);
+
+// Get restaurant orders
+router.get(
+  "/restaurant/:restaurantId",
+  authenticate,
+  authorize("MERCHANT", "ADMIN"),
+  orderController.getRestaurantOrders
+);
+
+// Update order status
+router.patch(
+  "/:orderId/status",
+  authenticate,
+  authorize("MERCHANT", "ADMIN"),
+  orderController.updateOrderStatus
+);
+
+// Cancel order
+router.patch("/:orderId/cancel", authenticate, orderController.cancelOrder);
+
+// Add rating
+router.post(
+  "/:orderId/rating",
+  authenticate,
+  authorize("USER"),
+  orderController.addRating
+);
 
 export default router;
