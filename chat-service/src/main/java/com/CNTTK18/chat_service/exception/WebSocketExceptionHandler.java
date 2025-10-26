5231a789f1541111db22aa88fc56d4416c3908e0
buihuy13 <buiquochuytd@gmail.com>
2025-10-26 15:11:24 +0700

    small fix to chat-service

--- chat-service/src/main/java/com/CNTTK18/chat_service/exception/WebSocketExceptionHandler.java
+++ chat-service/src/main/java/com/CNTTK18/chat_service/exception/WebSocketExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.CNTTK18.chat_service.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -7,19 +9,23 @@ import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
 
 public class WebSocketExceptionHandler extends StompSubProtocolErrorHandler {
+    private static final Logger log = LoggerFactory.getLogger(WebSocketExceptionHandler.class);
     @Override
     public Message<byte[]> handleClientMessageProcessingError(Message<byte[]> clientMessage, Throwable ex) {
         
-        // Log lỗi ở đây
-        System.err.println("Lỗi khi xử lý message WebSocket: " + ex.getMessage());
+        String errorMsg = (ex != null && ex.getMessage() != null) 
+            ? ex.getMessage() 
+            : "Unknown error";
+        
+        log.error("WebSocket error: {}", errorMsg, ex);
 
-        if (clientMessage != null) {
-            System.err.println("Client message: " + new String(clientMessage.getPayload()));
+        if (clientMessage != null && clientMessage.getPayload() != null) {
+            log.debug("Client message: " + new String(clientMessage.getPayload()));
         }
 
         // Tạo một message lỗi để gửi lại cho client
         StompHeaderAccessor accessor = StompHeaderAccessor.create(StompCommand.ERROR);
-        accessor.setMessage(ex.getMessage());
+        accessor.setMessage(errorMsg);
         accessor.setLeaveMutable(true);
 
         return MessageBuilder.createMessage(new byte[0], accessor.getMessageHeaders());
