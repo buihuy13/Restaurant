3cb007ee25f0e6b3aa4236441206bfded86fced7
buihuy13 <buiquochuytd@gmail.com>
2025-12-04 22:04:53 +0700

    added id to jwt in oauth

--- user-service/src/main/java/com/CNTTK18/user_service/service/Oauth2LoginSuccessHandler.java
+++ user-service/src/main/java/com/CNTTK18/user_service/service/Oauth2LoginSuccessHandler.java
@@ -2,13 +2,17 @@ package com.CNTTK18.user_service.service;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.CNTTK18.Common.Exception.ResourceNotFoundException;
 import com.CNTTK18.user_service.data.Role;
+import com.CNTTK18.user_service.model.Users;
+import com.CNTTK18.user_service.repository.UserRepository;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +22,8 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Oauth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     @Value("${frontend.url}")
     private String frontendUrl;
+    @Autowired
+    private UserRepository userRepository;
 
     private final JwtService jwtService;
     public Oauth2LoginSuccessHandler(JwtService jwtService) {
@@ -31,8 +37,9 @@ public class Oauth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         // Lấy thông tin user đã được xác thực
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getAttribute("email");
+        Users user = userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
-        String accessToken = jwtService.generateToken(email, Role.USER.toString());
+        String accessToken = jwtService.generateToken(email, Role.USER.toString(), user.getId());
         String redirectUrl = frontendUrl+ "?token=" + accessToken;
 
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
