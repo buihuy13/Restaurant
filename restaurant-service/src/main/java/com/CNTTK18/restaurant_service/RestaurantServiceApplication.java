0adf5e9dfd5cf4142716466b6786df569682d991
buihuy13 <buiquochuytd@gmail.com>
2025-12-07 19:49:58 +0700

    added scan Common packages

--- restaurant-service/src/main/java/com/CNTTK18/restaurant_service/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/CNTTK18/restaurant_service/RestaurantServiceApplication.java
@@ -3,7 +3,7 @@ package com.CNTTK18.restaurant_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.CNTTK18.restaurant_service, com.CNTTK18.Common")
 public class RestaurantServiceApplication {
 
 	public static void main(String[] args) {
