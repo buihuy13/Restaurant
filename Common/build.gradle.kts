1bfd390f345cf1b2bef5c230bfc556c993788b84
buihuy13 <buiquochuytd@gmail.com>
2025-12-07 14:25:50 +0700

    change Common module to plainJar

--- Common/build.gradle.kts
+++ Common/build.gradle.kts
@@ -5,3 +5,12 @@ plugins {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 }
+
+tasks.bootJar {
+	enabled = false
+}
+
+// Bật jar task để build plain JAR
+tasks.jar {
+	enabled = true
+}
