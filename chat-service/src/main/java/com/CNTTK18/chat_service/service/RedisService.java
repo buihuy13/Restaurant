2f48dce07e4865e66a718c5c630ea98928024740
buihuy13 <buiquochuytd@gmail.com>
2025-12-22 22:07:45 +0700

    added redis service for one time token checking

---
+++ chat-service/src/main/java/com/CNTTK18/chat_service/service/RedisService.java
@@ -0,0 +1,35 @@
+package com.CNTTK18.chat_service.service;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+    private final RedisTemplate<String, Boolean> redisTemplate;
+
+    public RedisService(RedisTemplate<String, Boolean> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void setValue(String key, Boolean value) {
+        redisTemplate.opsForValue().set(key, value, 30, TimeUnit.SECONDS); // Time to live: 30 giây
+    }
+
+    public Boolean getValue(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteValue(String key) {
+        redisTemplate.delete(key);
+    }
+
+    // Kiểm tra xem token đó đã được dùng chưa
+    @SuppressWarnings("null")
+    public Boolean exists(String key) {
+        return redisTemplate.hasKey(key) && redisTemplate.opsForValue().get(key).equals(true);
+    }
+}
+
+
